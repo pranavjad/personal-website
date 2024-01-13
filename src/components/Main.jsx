8d60f1adff8a146e7f2732b03da4d29cbdc1ddca
Pranav Jadhav <pra168109@gmail.com>
2024-01-13 01:50:22 -0500

    Changed some font sizes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,13 +27,13 @@ function Main() {
             <h2 className='text-4xl font-bold mb-4'>
                 About Me
             </h2>
-            <div className='mb-4 text-base'>
+            <div className='mb-4 text-2xl'>
                 I am currently studying CS with the machine intelligence and software engineering tracks at Purdue University.
                 I'm involved with Purdue Hackers where I work on technical projects every friday at hack night and
                 ML@Purdue where i'm the project lead for Purdue's TE AI Cup team. Additionally I am involved in Purdue's datamine
                 where I am working with SuperfluidDX to develop machine learning models to predict Alzheimer's Disease.
             </div>
-            <div className='text-1xl'>
+            <div className='text-2xl'>
                 Resume
             </div>
         </section>
@@ -46,29 +46,29 @@ function Main() {
             </h2>
             <ul className='grid grid-columns-1 gap-x-4 gap-y-11'>
                 <li className=''>
-                    <div className='bg-gradient-to-r from-sky-500 to-indigo-500 rounded-[20px]'>
+                    <div className='bg-gradient-to-r from-sky-200 to-indigo-200 rounded-[20px]'>
                         <img src="/Browser_Puzzle.png" alt="Picture of the browser puzzle website" className='rounded mb-2 scale-75' />
                     </div>
-                    <h2 className='text-2xl font-bold'>Browser Puzzle</h2>
-                    <span className=''>
+                    <h2 className='text-3xl font-bold py-2'>Browser Puzzle</h2>
+                    <span className='text-2xl'>
                         A website to solve puzzles in the browser. Built using p5.js.
                     </span>
                 </li>
                 <li>
-                <div className='bg-gradient-to-r from-sky-500 to-indigo-500 rounded-[20px]'>
+                <div className='bg-gradient-to-r from-sky-200 to-indigo-200 rounded-[20px]'>
                     <img src="/Multiplayer_Wordle.png" alt="Picture of the Multiplayer Wordle website" className='rounded mb-2 scale-75' />
                 </div>
-                    <h2 className='text-2xl font-bold'>Multiplayer Wordle</h2>
-                    <span className=''>
+                    <h2 className='text-3xl font-bold py-2'>Multiplayer Wordle</h2>
+                    <span className='text-2xl'>
                         Multiplayer wordle game made for the MIT Blueprint hackathon. Won 3rd place!
                     </span>
                 </li>
                 <li>
-                <div className='bg-gradient-to-r from-sky-500 to-indigo-500 rounded-[20px]'>
+                <div className='bg-gradient-to-r from-sky-200 to-indigo-200 rounded-[20px]'>
                     <img src="/Ethereum_Dashboard.png" alt="Picture of the ethereum dashboard page" className='rounded mb-2 scale-75' />
                 </div>
-                    <h2 className='text-2xl font-bold'>Ethereum Dashboard</h2>
-                    <span className=''>
+                    <h2 className='text-3xl font-bold py-2'>Ethereum Dashboard</h2>
+                    <span className='text-2xl'>
                         Ethereum data web dashboard with custom dataset scraped from a multitude of free API sources.
                     </span>
                 </li>
